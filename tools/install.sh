e463b29350bf66169c911cad4da08df00bd90b3c
Lars Kellogg-Stedman <lars@oddbit.com>
2024-11-24 20:00:49 -0500

    Correct problems in install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-service mysql start
+mysql_install_db --user=mysql --datadir=/var/lib/mysql --basedir=/usr
 
-mysql_install_db --initialize --user=root --user=$MARIADB_USER --datadir=? --basedir=?
+echo "Starting temporary server"
+mysqld --skip-networking --default-time-zone=SYSTEM --socket="/var/run/mysqld/mysqld.sock" --wsrep_on=OFF \
+		--expire-logs-days=0 \
+		--skip-slave-start \
+		--loose-innodb_buffer_pool_load_at_startup=0 \
+		--skip-ssl &
+
+echo "Waiting for database startup"
+while :; do
+  echo -n '.'
+  if mysql --database=mysql --skip-ssl <<<'SELECT 1' &> /dev/null; then
+    break
+  fi
+  sleep 1
+done
+echo
 
 echo "Creating user and db..."
 cat << EOF > /etc/mysql/init.sql
@@ -14,7 +29,8 @@ EOF
 mysql < /etc/mysql/init.sql
 
 echo "Database and user successfully created."
-
+echo "Stopping temporary server"
 kill $(cat /var/run/mysqld/mysqld.pid)
 
+echo "Starting persistent server"
 exec mysqld
